4d727b168941c8a6414f780c9d33890c3f1c28c7
Wojciech Kulma <wojciech.kulma@cern.ch>
2017-07-18 17:23:26 +0000

    Update property.py

--- cern_webinfra_inventory_client/property.py
+++ cern_webinfra_inventory_client/property.py
@@ -3,9 +3,21 @@ from exceptions import UnmatchedPropertyType
 
 
 class Property:  # TODO: !!! PROPER DESERIALIZATION WITH A TYPE SYSTEM !!!
+    
+    TYPES_MAPPING = {
+        'integer':  int,
+        'string':   str,
+        'boolean':  bool,
+        'date':     datetime.date,
+        'datetime': datetime.datetime,
+        'choice':   str,
+    }
+    
     def __init__(self, spec):
         self.spec = spec
-        self.type = self._get_type(spec['type'])
+        self.type = self.TYPES_MAPPING.get(spec['type'], None)
+        if not self.type: raise UnmatchedPropertyType(t)
+
         self.required = bool(spec['required'])
         self.read_only = bool(spec['read_only'])
         self.label = str(spec['label'])
@@ -14,16 +26,6 @@ class Property:  # TODO: !!! PROPER DESERIALIZATION WITH A TYPE SYSTEM !!!
             self.min_value = int(spec['max_value'])
         elif type(self.type) is str:
             self.max_length = spec['max_length']
-
-    @staticmethod
-    def _get_type(t):
-        if t == 'integer':  return int
-        if t == 'string':   return str
-        if t == 'boolean':  return bool
-        if t == 'date':     return datetime.date
-        if t == 'datetime': return datetime.datetime
-        if t == 'choice':   return str
-        raise UnmatchedPropertyType(t)
-
+            
     def __str__(self):
         return str(self.spec)
