11534597e356c5ad448c7af0d3de0a0c93f6a567
Wojciech Kulma <wojciech.kulma@cern.ch>
2017-10-13 15:26:05 +0200

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 setup(
     name='cern_webinfra_inventory_client',
     packages=['cern_webinfra_inventory_client'],
-    version='0.0.2',
+    version='0.0.3',
     description='Library for managing CERN web infrastructure inventory',
     author='CERN / Wojciech Kulma',
     author_email='wojciech.kulma@cern.ch',
